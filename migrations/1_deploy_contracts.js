d3139e7a11fc106b5c73bd8792159a81d0324b0b
Ninh Le <levanninh0101@gmail.com>
2021-04-21 22:28:44 +0700

    initital commit

---
+++ migrations/1_deploy_contracts.js
@@ -0,0 +1,27 @@
+const Token = artifacts.require('Token');
+const ICO = artifacts.require('ICO');
+
+module.exports = async function (deployer) {
+  const totalSupply = '10000000'; //10M
+  const availableTokens = '5000000'; //5M
+  await deployer.deploy(
+    Token,
+    'SafeDoge',
+    'SAFEDOGE',
+    web3.utils.toWei(totalSupply)
+  );
+  const token = await Token.deployed();
+
+  await deployer.deploy(
+    ICO,
+    token.address,
+    592200,                         // duration (592200s = 1 week)
+    web3.utils.toWei('2', 'milli'), // price of 1 token in DAI (wei) (= 0.002 DAI. 0.002 * 10M = 20,000 DAI ~= 20,000 USD)
+    web3.utils.toWei(availableTokens), //_availableTokens for the ICO. can be less than maxTotalSupply
+    200,                            //_minPurchase (in DAI)
+    5000                            //_maxPurchase (in DAI)
+  );
+  const ico = await ICO.deployed();
+  await token.updateAdmin(ico.address);
+  await ico.start();
+};
