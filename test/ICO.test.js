60c1394ef5f29cee2bf51f9ba83c7aa9bc88447e
Ninh Le <levanninh0101@gmail.com>
2021-04-22 05:38:24 +0700

    ICO test

--- test/ICO.test.js
+++ test/ICO.test.js
@@ -1,9 +1,18 @@
 const ICO = artifacts.require('ICO');
 
 contract('ICO', () => {
+  let ico = null;
+  before(async () => {
+    ico = await ICO.deployed();
+})
   it('Should deploy smart contract properly', async () => {
-    const ico = await ICO.deployed();
     console.log(ico.address);
     assert(ico.address != '');
+  })
+  it('Should have avaible tokens for ICO', async () => {
+    let availableTokens = await ico.availableTokens();
+    availableTokens = availableTokens.toString();
+    console.log("availableTokens ", availableTokens);
+    assert(web3.utils.fromWei(availableTokens , 'ether') == '5000000');
   })
 })
